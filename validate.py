533e124c7df0a07d049e0bee70c41b16d5945a5c
qiucheng1816 <46704658+qiucheng1816@users.noreply.github.com>
2023-02-08 18:11:23 +0800

    Create validate.py

---
+++ validate.py
@@ -0,0 +1,30 @@
+# true false
+# int 0 = false 
+# others = true
+print(bool(0))
+# false
+print(bool(1))
+# true
+print(bool(-1))
+# true
+
+# float 0.0 = false
+# float 1.1 = true
+
+# empty string = true
+words=''
+print(bool(words)
+# true
+
+# space and tab not empty,so will print true
+words = ' '
+print(bool(words))
+# true
+
+# otherwise will print true
+print(bool('a'))
+# true
+
+# if value is None will print false
+print(bool(None))
+# false
